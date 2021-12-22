5b8da3baa5e122820fc774113ea8db1ad270fe40
jennielai <jennie.t.lai001@gmail.com>
2021-12-21 20:59:17 -0500

    added blog/auth provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Main from "./components/Main";
+import { AuthProvider } from "./context/AuthContext";
+import { BlogProvider } from "./context/BlogContext";
+import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
+
+const breakpoints = ["910px", "1050px", "1100px", "1440px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const customTheme = {
+  ...theme,
+  breakpoints
+
+  // colors: {
+  //   ...theme.colors,
+  //   brand: {
+  //     900: "#1a365d",
+  //     800: "#153e75",
+  //     700: "#2a69ac"
+  //   }
+  // }
+};
+
+const config = theme => ({
+  light: {
+    color: theme.colors.gray[700],
+    bg: theme.colors.gray[100],
+    borderColor: theme.colors.gray[200],
+    placeholderColor: theme.colors.gray[500]
+  },
+  dark: {
+    color: theme.colors.whiteAlpha[900],
+    bg: theme.colors.gray[800],
+    borderColor: theme.colors.whiteAlpha[300],
+    placeholderColor: theme.colors.whiteAlpha[400]
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <BlogProvider>
+        </BlogProvider>
+      </AuthProvider>
+    </ChakraProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { theme, ChakraProvider, CSSReset } from "@chakra-ui/react";
+
+const breakpoints = ["360px", "768px", "1024px", "1440px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const newTheme = {
+  ...theme,
+  breakpoints
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={newTheme}>
+      <CSSReset />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+// function App() {
+//   return (
+//     <ChakraProvider theme={newTheme}>
+//       <CSSReset />
+//       <Header />
+//       <App />
+//     </ChakraProvider>
+//   );
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
