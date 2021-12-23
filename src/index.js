b4d9e95bb3da50be7d6a80ee1b8ded826f42b504
jennielai <jennie.t.lai001@gmail.com>
2021-12-22 19:56:43 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { theme, ChakraProvider, CSSReset } from "@chakra-ui/react";
-
-const breakpoints = ["360px", "768px", "1024px", "1440px"];
-breakpoints.sm = breakpoints[0];
-breakpoints.md = breakpoints[1];
-breakpoints.lg = breakpoints[2];
-breakpoints.xl = breakpoints[3];
-
-const newTheme = {
-  ...theme,
-  breakpoints
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={newTheme}>
-      <CSSReset />
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-// function App() {
-//   return (
-//     <ChakraProvider theme={newTheme}>
-//       <CSSReset />
-//       <Header />
-//       <App />
-//     </ChakraProvider>
-//   );
-// }
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<App />, rootElement);
